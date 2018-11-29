9b63e41e2fb5e7e5a083a90ca73fcf95b723b061
SaliiRV <43078265+SaliiRV@users.noreply.github.com>
2018-11-30 00:23:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 namespace SROMLab1 {
     class Program {
         static void Main(string[] args) {
-            string a = "95D0AC765C6D01F15A75CEA154AA5BC3F636459F925D6602255FF75DD3AD78D9";
-            string b = "DA9CEA567FAF76EFA1920FB35E1238AE8728B7B2EEE03797BAA757B06A45B8F";
+            string a = "FC50A8931F8CB0CD85369F87B019C86870ECF8DFD49EAA92178FC8E35DE636659D4C452A31D4ED99285F86145223A2CC5150DCC0498D6D6ED91B20BF1546104BE42260E2F9FA2538731393FFF232FA7107BA42088EB51ED8D663A40ED659C65FE709538EE0AA2CC90E4692E1125DF1695435F01193A63A527DF166785E659261";//"95D0AC765C6D01F15A75CEA154AA5BC3F636459F925D6602255FF75DD3AD78D9";
+            string b = "C5B3A5771ABD186AEFBC42FCEBCCD2D8D68695BEE690C988F750AF66F4B57EC842574C10BD33759823174B9B5CD342ADB1C665453F717B786F6C2AB16DEABE04DEAF2A7ADED9FD7567D39D87A309AE22476A165DDFFB3F8C986E58CB300CBAD10EBFF9C4C7BE7AA901F75C9D7E9DC44A565F085E70B30EEDE2CA5A83F8E052F8";//"DA9CEA567FAF76EFA1920FB35E1238AE8728B7B2EEE03797BAA757B06A45B8F";
             string c = "BFAFB3728B85B300F5AC85C52198659F903E1DAC8DF57600B0955C300AB850AC";
-            var a_32 = new ulong[16];
-            var b_32 = new ulong[16];
-            var c_32 = new ulong[16];
+            var a_32 = new ulong[128];
+            var b_32 = new ulong[128];
+            var c_32 = new ulong[128];
             ToArr(a, a_32);
             ToArr(b, b_32);
             ToArr(c, c_32);
@@ -41,12 +41,12 @@ namespace SROMLab1 {
         }
 
         public static void ToArr(string a, ulong[] a_32) {
-            var p_32 = new ulong[16];
+            var p_32 = new ulong[128];
             string z = "0";
-            while (a.Length != 128) {
+            while (a.Length != 256) {
                 a = z + a;
             }
-            for (int i = 0; i < a.Length; i += 8) { 
+            for (int i = 0; i < a.Length; i += 8) {
                 p_32[i / 8] = Convert.ToUInt64(a.Substring(i, 8), 16);
                 a_32[i / 8] = p_32[i / 8];
             }
@@ -79,7 +79,7 @@ namespace SROMLab1 {
 
         public static ulong[] Subtraction(ulong[] a_32, ulong[] b_32) {
             ulong borrow = 0;
-            var c = new ulong[16];
+            var c = new ulong[128];
             for (var i = 0; i < a_32.Length; i++) {
                 var temp = a_32[i] - b_32[i] - borrow;
                 if (temp > a_32[i]) {
@@ -96,7 +96,7 @@ namespace SROMLab1 {
 
         public static ulong[] Multiply(ulong[] a_32, ulong[] b_32) {
             ulong carry = 0;
-            var c = new ulong[32];
+            var c = new ulong[256];
             for (int i = 0; i < a_32.Length; i++) {
                 carry = 0;
                 for (int j = 0; j < b_32.Length; j++) {
@@ -109,11 +109,11 @@ namespace SROMLab1 {
             return c;
         }
 
-        public static ulong[] Division(ulong[] a_32, ulong[] b_32/*, ulong[] x_32*/) {
-            var b = new ulong[16];
+        public static ulong[] Division(ulong[] a_32, ulong[] b_32) {
+            var b = new ulong[128];
             int k = BitLength(b_32);
             var x = a_32;
-            var q = new ulong[16];
+            var q = new ulong[128];
             while (Cmp(x, b_32) >= 0) {
                 int t = BitLength(x);
                 b = ShiftBitsToHigh(b_32, t - k);
@@ -149,8 +149,8 @@ namespace SROMLab1 {
 
         public static ulong[] ShiftBitsToHigh(ulong[] b_32, int shift_num) {
             if (shift_num == 0) { return b_32; };
-            var c = new ulong[16];
-            var surrogate = new ulong[16];
+            var c = new ulong[128];
+            var surrogate = new ulong[128];
             Array.Copy(b_32, surrogate, b_32.Length);
             int shift;
             while (shift_num > 0) {
@@ -180,7 +180,7 @@ namespace SROMLab1 {
         }
 
         public static ulong[] SetBit(ulong[] a_32, int position) {
-            var temp = new ulong[16];
+            var temp = new ulong[128];
             temp[0] = 1;
             temp = ShiftBitsToHigh(temp, position);
             return Addition(a_32, temp);
